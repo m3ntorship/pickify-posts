71b27193d3283372c4017bb1a732c29f3f1d72cc
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-05-09 22:37:40 +0200

    fixed failing tests

--- src/votes/votes.controller.spec.ts
+++ src/votes/votes.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VotesController } from './votes.controller';
+import { VotesService } from './votes.service';
 
 describe('VotesController', () => {
   let controller: VotesController;
@@ -7,6 +8,7 @@ describe('VotesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [VotesController],
+      providers: [VotesService],
     }).compile();
 
     controller = module.get<VotesController>(VotesController);
