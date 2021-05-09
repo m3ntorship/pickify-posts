036e788b5352b0e6264cb77cda84c56f2c316095
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-05-09 23:14:19 +0200

    fixed failing tests - added test for every function

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,5 +1,7 @@
+import { NotImplementedException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
 
 describe('PostsController', () => {
   let controller: PostsController;
@@ -7,6 +9,7 @@ describe('PostsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostsController],
+      providers: [PostsService],
     }).compile();
 
     controller = module.get<PostsController>(PostsController);
@@ -14,5 +17,47 @@ describe('PostsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('createPost function', () => {
+    it('should throw not implemented', () => {
+      expect(controller.createPost).toThrowError(new NotImplementedException());
+    });
+  });
+
+  describe('getAllPosts function', () => {
+    it('should throw not implemented', () => {
+      expect(controller.getAllPosts).toThrowError(
+        new NotImplementedException(),
+      );
+    });
+  });
+
+  describe('getSinglePost function', () => {
+    it('should throw not implemented', () => {
+      expect(controller.getSinglePost).toThrowError(
+        new NotImplementedException(),
+      );
+    });
+  });
+
+  describe('createOptionGroup function', () => {
+    it('should throw not implemented', () => {
+      expect(controller.createOptionGroup).toThrowError(
+        new NotImplementedException(),
+      );
+    });
+  });
+
+  describe('flagPost function', () => {
+    it('should throw not implemented', () => {
+      expect(controller.flagPost).toThrowError(new NotImplementedException());
+    });
+  });
+
+  describe('deletePost function', () => {
+    it('should throw not implemented', () => {
+      expect(controller.deletePost).toThrowError(new NotImplementedException());
+    });
   });
 });
