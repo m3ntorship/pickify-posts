44f494ba20f6e2913859b5386a2eda89417812b6
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-05-09 23:04:26 +0200

    added testing for function throwing

--- src/votes/votes.controller.spec.ts
+++ src/votes/votes.controller.spec.ts
@@ -1,3 +1,4 @@
+import { NotImplementedException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { VotesController } from './votes.controller';
 import { VotesService } from './votes.service';
@@ -16,5 +17,9 @@ describe('VotesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should have addVote method', () => {
+    expect(controller.addVote).toThrowError(new NotImplementedException());
   });
 });
