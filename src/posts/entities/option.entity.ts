514775a9ef849af1ba6c0b1cc6d1fde4dccc3f94
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-05-10 17:49:09 +0200

    added option entity

--- src/posts/entities/option.entity.ts
+++ src/posts/entities/option.entity.ts
@@ -1,3 +1,17 @@
 import Model from 'src/shared/entity.model';
+import { Column, ManyToOne } from 'typeorm';
+import { OptionGroup } from './optiongroup.entity';
 
-export class Option extends Model {}
+export class Option extends Model {
+  @Column()
+  body: string;
+
+  @Column()
+  vote_count: number;
+
+  @Column()
+  group_id: number;
+
+  @ManyToOne(() => OptionGroup, (optiongroup) => optiongroup.options)
+  optiongroup: OptionGroup;
+}
