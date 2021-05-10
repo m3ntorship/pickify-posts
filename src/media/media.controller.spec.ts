c560b375ee479d9073f75f178d1187034a60eba5
Amr Akmal <amrakmal97@gmail.com>
2021-05-10 17:13:45 +0200

    Add tests for addMedia function

--- src/media/media.controller.spec.ts
+++ src/media/media.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MediaController } from './media.controller';
 import { MediaService } from './media.service';
+import { NotImplementedException } from '@nestjs/common';
 
 describe('MediaController', () => {
   let controller: MediaController;
@@ -16,5 +17,11 @@ describe('MediaController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('addMedia function', () => {
+    it('should throw not implemented', () => {
+      expect(controller.addMedia()).toThrowError(new NotImplementedException());
+    });
   });
 });
