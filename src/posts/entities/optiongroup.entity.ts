d1b468e721e5b2440644fda5010a587210c8a577
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-05-10 17:50:49 +0200

    added relation in optiongroup entity with option entity

--- src/posts/entities/optiongroup.entity.ts
+++ src/posts/entities/optiongroup.entity.ts
@@ -1,3 +1,8 @@
+import { OneToMany } from 'typeorm';
 import Model from 'src/shared/entity.model';
+import { Option } from './option.entity';
 
-export class OptionGroup extends Model {}
+export class OptionGroup extends Model {
+  @OneToMany(() => Option, (option) => option.optiongroup)
+  options: Option[];
+}
