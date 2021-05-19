cb429ccdad6b821263908887ae69d51c917f7b62
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-05-19 22:03:34 +0200

    fix: change example db name to "example"

--- src/moduleExample/entities/moduleExample.entity.ts
+++ src/moduleExample/entities/moduleExample.entity.ts
@@ -2,7 +2,7 @@ import { Entity, Column } from 'typeorm';
 
 import Model from '../../shared/entity.model';
 
-@Entity('posts')
+@Entity('example')
 export class ModuleExample extends Model {
   @Column()
   col1: string;
