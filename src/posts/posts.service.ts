69baa5df90455399a3231e31bf69177663e72ba8
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-05-19 22:07:40 +0200

    Add createPost service function

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
+import { PostRepository } from './entities/postRepository';
 import type { CreatePost as CreatePostInterface } from './interfaces/createPost.interface';
 
 @Injectable()
 export class PostsService {
+  constructor(private postRepository: PostRepository) {}
   async createPost(createPostDto: CreatePostDto): Promise<CreatePostInterface> {
-    return { id: 'test' };
+    const createdPost = await this.postRepository.createPost(createPostDto);
+    return { id: createdPost.uuid };
   }
 }
