384621e564180d8833336d3dc3b093374c1516d7
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-05-19 22:32:44 +0200

    add validation to create-post-dto

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,16 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilterLogger } from './logging/http-exceptions-logger.filter';
 import { winstonLoggerOptions } from './logging/winston.options';
 import { LoggingInterceptor } from './logging/logging.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // add global prefix to all endpoints
   app.setGlobalPrefix('api');
+
+  // add global validation pipe
+  app.useGlobalPipes(new ValidationPipe());
 
   // Enable cors
   app.enableCors();
--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,5 +1,11 @@
+import { IsBoolean, IsIn } from 'class-validator';
+
 export class CreatePostDto {
-  caption: string;
-  type: string;
+  caption?: string;
+
+  @IsIn(['text_poll', 'image_poll', 'mini_survey'])
+  type: 'text_poll' | 'image_poll' | 'mini_survey';
+
+  @IsBoolean()
   is_hidden: boolean;
 }
