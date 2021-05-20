0f06ce0c7f0775cf9b3b4a802219505cc3e50a09
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-05-21 00:53:35 +0200

    Add options_group entity

---
+++ src/posts/entities/optionsGroup.entity.ts
@@ -0,0 +1,12 @@
+// import { OneToMany } from 'typeorm';
+import Model from 'src/shared/entity.model';
+import { Column, Entity } from 'typeorm';
+// import { Option } from './option.entity';
+
+@Entity('options_groups')
+export class OptiosnGroup extends Model {
+  // @OneToMany(() => Option, (option) => option.optiongroup)
+  // options: Option[];
+  @Column()
+  name: string;
+}
