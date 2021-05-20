81c890cb966c0dad2d55dc9afea9053242b0e21a
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-05-20 22:47:29 +0200

    implement flagPostCreation repository  method

--- src/posts/entities/postRepository.ts
+++ src/posts/entities/postRepository.ts
@@ -1,9 +1,13 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { BadRequestException, HttpException, HttpStatus } from '@nestjs/common';
 import { CreatePostDto } from '../dto/create-post.dto';
 import { Post } from './post.entity';
 
 @EntityRepository(Post)
 export class PostRepository extends Repository<Post> {
+  /**
+   * createPost
+   */
   public async createPost(createPostDto: CreatePostDto): Promise<Post> {
     const { caption, type, is_hidden } = createPostDto;
     const post = this.create();
@@ -15,5 +19,15 @@ export class PostRepository extends Repository<Post> {
     post.ready = false;
     await this.save(post);
     return post;
+  }
+
+  /**
+   * flagPostCreation
+   */
+  public async flagPostCreation(flag: boolean, postid: string) {
+    const post = await this.findOne({ where: { uuid: postid } });
+    if (!post) throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    post.created = flag;
+    await this.save(post);
   }
 }
