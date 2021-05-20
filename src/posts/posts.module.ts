56a492576ef35728268ddfdd18a9ce4267f018e3
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-05-21 01:28:55 +0200

    Add option repository + import it in post module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -4,9 +4,16 @@ import { PostRepository } from './entities/postRepository';
 import { OptionsGroupRepository } from './entities/optionsGroup.repository';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { OptionRepository } from './entities/option.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostRepository, OptionsGroupRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      PostRepository,
+      OptionsGroupRepository,
+      OptionRepository,
+    ]),
+  ],
   controllers: [PostsController],
   providers: [PostsService],
 })
