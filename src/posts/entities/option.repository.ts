4ae674d67659e3b78e3f970d6a7590f0cdd89781
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-05-27 17:35:45 +0200

    Add options & groups repositories

--- src/posts/entities/option.repository.ts
+++ src/posts/entities/option.repository.ts
@@ -1,5 +1,22 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Option } from './option.entity';
+import { OptiosnGroup } from './optionsGroup.entity';
+import { OptionDto } from '../dto/optionGroupCreation.dto';
 
 @EntityRepository(Option)
-export class OptionRepository extends Repository<Option> {}
+export class OptionRepository extends Repository<Option> {
+  /**
+   * createOption   */
+  public async createOption(
+    group: OptiosnGroup,
+    optionData: OptionDto,
+  ): Promise<Option> {
+    const option = this.create();
+    option.optionsGroup = group;
+    option.body = optionData.body;
+    option.vote_count = optionData.vote_count;
+    // group.post = post;
+    await option.save();
+    return option;
+  }
+}
--- src/posts/entities/optionsGroup.repository.ts
+++ src/posts/entities/optionsGroup.repository.ts
@@ -1,5 +1,23 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { OptiosnGroup } from './optionsGroup.entity';
+import { Post } from './post.entity';
 
 @EntityRepository(OptiosnGroup)
-export class OptionsGroupRepository extends Repository<OptiosnGroup> {}
+export class OptionsGroupRepository extends Repository<OptiosnGroup> {
+  /**
+   * createGroup
+   */
+  public async createGroup(
+    postid: string,
+    name: string,
+  ): Promise<OptiosnGroup> {
+    // const post = await this.manager
+    //   .getRepository(Post)
+    //   .findOne({ where: { uuid: postid } });
+    const group = this.create();
+    group.name = name;
+    // group.post = post;
+    await group.save();
+    return group;
+  }
+}
