b19ad24a6fb099cd7aa8edadf09011e09e46ddc8
Amr Akmal <amrakmal97@gmail.com>
2021-06-06 14:55:33 +0200

    Add an extra needed test

--- src/utils/LoggingUtils/generateErrorBasedOnEnvironment.spec.ts
+++ src/utils/LoggingUtils/generateErrorBasedOnEnvironment.spec.ts
@@ -23,7 +23,7 @@ describe('test error returned based on the working environment', () => {
     ).toEqual(expectedErrorInNonProductionEnv);
   });
 
-  it('check error returned if env is production', () => {
+  it('check error returned if env is production and error from server', () => {
     expect(
       generateErrorBasedOnCurrentEnvironment(
         500,
@@ -32,5 +32,20 @@ describe('test error returned based on the working environment', () => {
         'production',
       ),
     ).toEqual(expectedErrorInProductionEnv);
+  });
+
+  it('check error returned if env is production and error is not from server', () => {
+    expect(
+      generateErrorBasedOnCurrentEnvironment(
+        400,
+        'Error Message',
+        'stack trace',
+        'production',
+      ),
+    ).toEqual({
+      ...expectedErrorInProductionEnv,
+      statusCode: 400,
+      message: 'Error Message',
+    });
   });
 });
