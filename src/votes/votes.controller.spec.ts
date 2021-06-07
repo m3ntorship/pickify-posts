6ec07e0c5a04edd0e16a896ba032bad4801bd697
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-06-07 07:04:54 +0200

    finish tests

--- src/votes/votes.controller.spec.ts
+++ src/votes/votes.controller.spec.ts
@@ -3,13 +3,20 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { VotesController } from './votes.controller';
 import { VotesService } from './votes.service';
 
+const mockService = { addVote: jest.fn(() => Promise.resolve(['options'])) };
+
 describe('VotesController', () => {
   let controller: VotesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [VotesController],
-      providers: [{ provide: VotesService, useValue: { addVote: jest.fn() } }],
+      providers: [
+        {
+          provide: VotesService,
+          useValue: mockService,
+        },
+      ],
     }).compile();
 
     controller = module.get<VotesController>(VotesController);
@@ -19,9 +26,15 @@ describe('VotesController', () => {
     expect(controller).toBeDefined();
   });
 
-  // describe('addVote function', () => {
-  //   it('should throw not implemented', () => {
-  //     expect(controller.addVote).toThrowError(new NotImplementedException());
-  //   });
-  // });
+  describe('addVote', () => {
+    const params = { optionId: 'uuid' };
+    it('should call service function with optionId', async () => {
+      controller.addVote(params.optionId);
+      expect(mockService.addVote).toBeCalledWith(params.optionId);
+    });
+    it('should return whatever service method returns', () => {
+      const res = controller.addVote(params.optionId);
+      expect(res).resolves.toEqual(['options']);
+    });
+  });
 });
