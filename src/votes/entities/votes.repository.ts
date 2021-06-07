2296ae76c4bec85f60ff54c3c7cff57fa7cc907a
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-06-07 05:24:01 +0200

    return response as per contract

--- src/votes/entities/votes.repository.ts
+++ src/votes/entities/votes.repository.ts
@@ -1,16 +1,19 @@
-import { Option } from '../../posts/entities/option.entity';
 import { EntityRepository, Repository } from 'typeorm';
-import { Vote } from './vote.entity';
 import {
   InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
+import { Vote } from './vote.entity';
+import { Option } from '../../posts/entities/option.entity';
 
 @EntityRepository(Vote)
 export class VoteRepository extends Repository<Vote> {
   async addVote(optionId: string) {
     try {
-      const option = await Option.findOneOrFail({ where: { uuid: optionId } });
+      const option = await Option.findOneOrFail({
+        where: { uuid: optionId },
+        relations: ['optionsGroup', 'optionsGroup.options'],
+      });
 
       const vote = new Vote();
       vote.option = option;
@@ -18,8 +21,12 @@ export class VoteRepository extends Repository<Vote> {
 
       option.vote_count++;
       await option.save();
+
+      const response = option.optionsGroup.options.map((option) => {
+        return { votes_count: option.vote_count, optionId: option.uuid };
+      });
+      return response;
     } catch (error) {
-      console.log(error);
       if (error.name === 'EntityNotFound') throw new NotFoundException();
       else throw new InternalServerErrorException();
     }
