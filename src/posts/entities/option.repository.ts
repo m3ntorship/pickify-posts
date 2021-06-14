fc9922eff0fcd40ab37f1bc6b205b9dea4eab51d
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-06-14 21:30:25 +0200

    Refactor: re-write vote repo and service to separate concerns + tests

--- src/posts/entities/option.repository.ts
+++ src/posts/entities/option.repository.ts
@@ -17,4 +17,35 @@ export class OptionRepository extends Repository<Option> {
     option.vote_count = 0;
     return await this.save(option);
   }
+
+  /**
+   * get option with relation to vote, grous & post
+   */
+
+  public async findOptionById(optionId: string): Promise<Option> {
+    return await this.createQueryBuilder('options')
+      .where('options.uuid = :optionId', { optionId })
+      .leftJoinAndSelect('options.votes', 'vote')
+      .leftJoinAndSelect('options.optionsGroup', 'optionsGroup')
+      .leftJoinAndSelect('optionsGroup.options', 'option')
+      .leftJoinAndSelect('optionsGroup.post', 'post')
+      .getOne();
+
+    // or using findOne
+    // return await this.findOne({
+    //   where: { uuid: optionId },
+    //   relations: [
+    //     'votes',
+    //     'optionsGroup',
+    //     'optionsGroup.options',
+    //     'optionsGroup.post',
+    //   ],
+    // });
+  }
+
+  public async incrementVoteCount(option: Option): Promise<Option> {
+    option.vote_count++;
+    await this.save(option);
+    return option;
+  }
 }
--- src/votes/entities/votes.repository.ts
+++ src/votes/entities/votes.repository.ts
@@ -1,58 +1,14 @@
 import { EntityRepository, Repository } from 'typeorm';
-import {
-  HttpStatus,
-  HttpException,
-  ConflictException,
-  NotFoundException,
-} from '@nestjs/common';
 import { Vote } from './vote.entity';
 import { Option } from '../../posts/entities/option.entity';
-import { OptionsVotes } from '../interfaces/optionsVotes.interface';
 
 @EntityRepository(Vote)
 export class VoteRepository extends Repository<Vote> {
-  async addVote(optionId: string, userId: number): Promise<OptionsVotes[]> {
-    const option = await Option.findOne({
-      where: { uuid: optionId },
-      relations: [
-        'votes',
-        'optionsGroup',
-        'optionsGroup.options',
-        'optionsGroup.post',
-      ],
-    });
-
-    // if option not found
-    if (!option)
-      throw new NotFoundException(`Option with id:${optionId} not found`);
-
-    // if post of option still under creation
-    if (!option.optionsGroup.post.created)
-      throw new HttpException(
-        `Post:${option.optionsGroup.post.uuid} with option:${optionId} still under creation...`,
-        423,
-      );
-
-    // check if user has voted for this option before
-    const isUserVoted = option.votes.some((vote) => vote.user_id === userId);
-    if (isUserVoted) {
-      throw new ConflictException('User has already voted for this option');
-    }
-
+  async addVote(option: Option, userId: number): Promise<void> {
     // Create vote and save it in DB
     const vote = this.create();
     vote.option = option;
     vote.user_id = userId;
-    await vote.save();
-
-    // increment the vote count in the option
-    option.vote_count++;
-    await option.save();
-
-    const response = option.optionsGroup.options.map((option) => {
-      if (option.uuid === optionId) option.vote_count++;
-      return { votes_count: option.vote_count, optionId: option.uuid };
-    });
-    return response;
+    await this.save(vote);
   }
 }
