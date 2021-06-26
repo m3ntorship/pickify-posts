68526f587e368bb9bd42e0fab2b9fd2537dbb4c2
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-06-26 14:40:53 +0200

    add media data in database

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,11 +1,22 @@
-import { Module, ValidationPipe } from '@nestjs/common';
-import { ClassTransformer } from 'class-transformer';
-import { Validator } from 'class-validator';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OptionRepository } from 'src/posts/entities/option.repository';
+import { OptionsGroupRepository } from 'src/posts/entities/optionsGroup.repository';
+import { PostRepository } from 'src/posts/entities/post.repository';
+import { MediaRepository } from './entities/media.repository';
 import { MediaController } from './media.controller';
 import { MediaService } from './media.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      MediaRepository,
+      PostRepository,
+      OptionsGroupRepository,
+      OptionRepository,
+    ]),
+  ],
   controllers: [MediaController],
-  providers: [MediaService, Validator, ClassTransformer, ValidationPipe],
+  providers: [MediaService],
 })
 export class MediaModule {}
--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -1,29 +1,55 @@
-import { Injectable, ValidationPipe } from '@nestjs/common';
-import { ClassTransformer } from 'class-transformer';
-import { Validator } from 'class-validator';
+import { Injectable } from '@nestjs/common';
+import { Option } from 'src/posts/entities/option.entity';
+import { OptionRepository } from 'src/posts/entities/option.repository';
+import { OptiosnGroup } from 'src/posts/entities/optionsGroup.entity';
+import { OptionsGroupRepository } from 'src/posts/entities/optionsGroup.repository';
+import { Post } from 'src/posts/entities/post.entity';
+import { PostRepository } from 'src/posts/entities/post.repository';
+import { mediaType } from 'src/shared/enums/mediaType.enum';
 import { MediaDataMessageDto } from './dto/mediaDataMessage-dto';
+import { MediaRepository } from './entities/media.repository';
 
 @Injectable()
 export class MediaService {
   constructor(
-    private classTransformer: ClassTransformer,
-    private validation: Validator,
-    private validationPipe: ValidationPipe,
+    private mediaRepo: MediaRepository,
+    private postRepo: PostRepository,
+    private optionRepo: OptionRepository,
+    private optionsGroupRepo: OptionsGroupRepository,
   ) {}
+
+  private async getEntity(
+    entityType: MediaDataMessageDto,
+  ): Promise<Post | OptiosnGroup | Option> {
+    switch (entityType.entity_type) {
+      case mediaType.POST:
+        return await this.postRepo.findOne({
+          where: { uuid: entityType.entity_id },
+        });
+
+      case mediaType.OPTION_GROUP:
+        return await this.optionsGroupRepo.findOne({
+          where: { uuid: entityType.entity_id },
+        });
+
+      case mediaType.OPTION:
+        return await this.optionRepo.findOne({
+          where: { uuid: entityType.entity_id },
+        });
+
+      default:
+        break;
+    }
+  }
+
   async handleMedia(mediaData: MediaDataMessageDto) {
-    // validate message
-    // const classTransformer = new ClassTransformer();
-    const entity = this.classTransformer.plainToClass(
-      MediaDataMessageDto,
-      mediaData,
-    );
+    // get the (post | group | option) from DB
+    const entity = await this.getEntity(mediaData);
 
-    const errors = await this.validation.validate(entity);
-    console.log(
-      'here to have the validation errors. Use it to throw error',
-      errors,
-    );
+    // add the data to media table in DB
+    await this.mediaRepo.add(entity, mediaData.file_url, mediaData.entity_type);
 
-    console.log(`mediaData-------------`, mediaData);
+    // change post rediness if all media files in post got handled
+    // TODO
   }
 }
