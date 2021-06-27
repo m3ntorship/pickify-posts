5cc0cf36e2ca35beb53afead89d2d30bf5461eec
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-06-27 18:03:17 +0200

    fix: the type of media_entity_type

--- src/media/dto/mediaDataMessage-dto.ts
+++ src/media/dto/mediaDataMessage-dto.ts
@@ -2,12 +2,12 @@ import { IsIn, IsUUID } from 'class-validator';
 import { mediaType } from '../../shared/enums/mediaType.enum';
 
 export class MediaDataMessageDto {
-  @IsUUID(4, { message: 'entity id is not uuid valid' })
+  @IsUUID(4, { message: 'file id is not uuid valid' })
   file_id: string;
 
   @IsUUID(4, { message: 'entity id is not uuid valid' })
   entity_id: string;
 
   @IsIn([mediaType.OPTION, mediaType.POST, mediaType.OPTION_GROUP])
-  entity_type: string;
+  entity_type: mediaType;
 }
