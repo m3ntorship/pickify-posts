72b06298c7414538a79235b4c4e4a13053d6f60a
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-06-28 11:40:25 +0200

    add RpcExceptionFilter to hanldle all RpcExceptions + logging

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,14 +1,24 @@
-import { Controller } from '@nestjs/common';
-import { EventPattern } from '@nestjs/microservices';
+import {
+  Controller,
+  UseFilters,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import { ExceptionFilter } from '../shared/exception-filters/rpc-exception.filter';
 import { MediaDataMessageDto } from './dto/mediaDataMessage-dto';
 import { MediaService } from './media.service';
+import * as winston from 'winston';
+import { winstonLoggerOptions } from 'src/logging/winston.options';
 
 @Controller('media')
+@UsePipes(new ValidationPipe())
+@UseFilters(new ExceptionFilter(winston.createLogger(winstonLoggerOptions)))
 export class MediaController {
   constructor(private mediaService: MediaService) {}
 
   @EventPattern('added_media_data')
-  async handleMedia(mediaData: MediaDataMessageDto): Promise<void> {
+  async handleMedia(@Payload() mediaData: MediaDataMessageDto): Promise<void> {
     await this.mediaService.handleMedia(mediaData);
   }
 }
