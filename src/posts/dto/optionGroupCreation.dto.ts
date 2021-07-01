3b7e9fe9733f3f27cbab40f54703b4d71e777b29
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-07-01 16:50:13 +0200

    add max optionsGroup & options limit in dto validation

--- src/posts/dto/optionGroupCreation.dto.ts
+++ src/posts/dto/optionGroupCreation.dto.ts
@@ -1,5 +1,10 @@
 import { Type } from 'class-transformer';
-import { ValidateNested, IsNotEmpty, IsOptional } from 'class-validator';
+import {
+  ValidateNested,
+  IsNotEmpty,
+  IsOptional,
+  ArrayMaxSize,
+} from 'class-validator';
 
 export class OptionDto {
   @IsNotEmpty()
@@ -10,13 +15,18 @@ export class OptionsGroupDto {
   @IsOptional()
   @IsNotEmpty()
   name?: string;
+
   @ValidateNested({ each: true })
   @Type(() => OptionDto)
+  @ArrayMaxSize(4, { message: `Max options number in a group is 4` })
   options: OptionDto[];
 }
 
 export class OptionsGroupCreationDto {
   @ValidateNested({ each: true })
   @Type(() => OptionsGroupDto)
+  @ArrayMaxSize(4, {
+    message: `Max groups number in a post is 4`,
+  })
   groups: OptionsGroupDto[];
 }
