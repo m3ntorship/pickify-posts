6dcf4608991d070e5dc5cb13d1bec5b22ac110b3
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-07-04 10:40:10 +0200

    moved all logic from FullAuthGuard to validate function

--- src/auth/firebase-auth.strategy.ts
+++ src/auth/firebase-auth.strategy.ts
@@ -2,6 +2,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { Strategy, ExtractJwt } from 'passport-firebase-jwt';
 import admin from 'firebase-admin';
+import { UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class FirebaseAuthStrategy extends PassportStrategy(Strategy) {
@@ -12,7 +13,18 @@ export class FirebaseAuthStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(token) {
-    const user = await admin.auth().verifyIdToken(token, true);
-    return user;
+    try {
+      const user = await admin.auth().verifyIdToken(token, true);
+      return user;
+    } catch (error) {
+      return this.staticIdAuth(token);
+    }
+  }
+  private staticIdAuth(uuid: string): boolean {
+    // Add logic later
+    if (!uuid) {
+      throw new UnauthorizedException('No user id');
+    }
+    return true;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import { LoggingInterceptor } from './logging/logging.interceptor';
 import { ValidationPipe } from '@nestjs/common';
 import admin from 'firebase-admin';
 import * as path from 'path';
-import { FullAuthGuard } from './auth/full_auth.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -46,7 +46,7 @@ async function bootstrap() {
   const logger = winston.createLogger(winstonLoggerOptions);
   app.useGlobalInterceptors(new LoggingInterceptor(logger));
   app.useGlobalFilters(new AllExceptionsFilterLogger(logger));
-  app.useGlobalGuards(new FullAuthGuard());
+  app.useGlobalGuards(new (AuthGuard('firebase-jwt'))());
 
   app.use('/health', (req: any, res: any) => {
     res.send({ status: true });
