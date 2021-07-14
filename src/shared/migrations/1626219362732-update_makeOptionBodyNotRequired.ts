11b54db9d1c69a86738cb4e3324c1b9969969483
Amr Akmal <amrakmal97@gmail.com>
2021-07-14 14:47:06 +0200

    modify pickify_posts with

--- src/shared/migrations/1626219362732-update_makeOptionBodyNotRequired.ts
+++ src/shared/migrations/1626219362732-update_makeOptionBodyNotRequired.ts
@@ -1,4 +1,5 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+import { POSTS_SCHEMA } from '../entity.model';
 
 export class updateMakeOptionBodyNotRequired1626219362732
   implements MigrationInterface {
@@ -6,19 +7,19 @@ export class updateMakeOptionBodyNotRequired1626219362732
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "pickify_posts"."options" ALTER COLUMN "body" DROP NOT NULL`,
+      `ALTER TABLE "${POSTS_SCHEMA}"."options" ALTER COLUMN "body" DROP NOT NULL`,
     );
     await queryRunner.query(
-      `COMMENT ON COLUMN "pickify_posts"."options"."body" IS NULL`,
+      `COMMENT ON COLUMN "${POSTS_SCHEMA}"."options"."body" IS NULL`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `COMMENT ON COLUMN "pickify_posts"."options"."body" IS NULL`,
+      `COMMENT ON COLUMN "${POSTS_SCHEMA}"."options"."body" IS NULL`,
     );
     await queryRunner.query(
-      `ALTER TABLE "pickify_posts"."options" ALTER COLUMN "body" SET NOT NULL`,
+      `ALTER TABLE "${POSTS_SCHEMA}"."options" ALTER COLUMN "body" SET NOT NULL`,
     );
   }
 }
